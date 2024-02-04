7b045211553c43370f33e6b9018453cae5c74bb4
Awais Riaz <74641603+WasiG-619@users.noreply.github.com>
2024-02-04 17:36:53 +0000

    Add files via upload

---
+++ js backup/jsFunctions.js
@@ -0,0 +1,3 @@
+var x = parseInt(document.getElementById("x").value);
+var y = parseInt(document.getElementById("y").value);
+document.getElementById("result").innerHTML = x + y;
---
+++ js/jsFunctions.js
@@ -0,0 +1,7 @@
+function myFunction() {
+
+	var x = parseInt(document.getElementById("x").value);
+	var y = parseInt(document.getElementById("y").value);
+	document.getElementById("result").innerHTML = x + y;
+
+}
